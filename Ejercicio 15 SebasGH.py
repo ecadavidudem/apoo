3c080fdc96bf0e7c9e61fe9e3896c79f557b816e
SebasGH <110042473+SebasGarciaHenao@users.noreply.github.com>
2022-07-31 16:14:36 -0500

    Add files via upload

---
+++ Ejercicio 15 SebasGH.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 31 15:57:12 2022
+
+@author: sebas
+"""
+
+n=int(input("Ingrese el número\n"))
+if n % 2==0:
+    print("El número",n," es par")
+else:
+    print("El número",n," es impar")
---
+++ Ejercicio 5 SebasGH.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 31 15:44:54 2022
+
+@author: sebas
+"""
+
+n1=float(input("Ingrese el primer número\n"))
+n2=float(input("Ingrese el segundo número\n"))
+mult=n1*n2
+print("El número de la multiplicación es", mult)
