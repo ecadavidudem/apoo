f8885f61f0e3418ec5f6a430ab0b452b9857275f
juanca0505 <110240690+juanca0505@users.noreply.github.com>
2022-08-23 08:48:34 -0500

    Add files via upload

---
+++ 1 inciso.py
@@ -0,0 +1,13 @@
+class Punto:
+    def __init__ (self, X, Y):
+        self.X = X
+        self.Y = Y
+        
+
+My_Coordenada = Punto(2,7)
+
+print("Ubicacion en X:")
+print(My_Coordenada.X)
+
+print("Ubicacion en Y:")
+print(My_Coordenada.Y)
---
+++ untitled1.py
@@ -0,0 +1,14 @@
+class Punto:
+    def _init_ (self, x, y):
+        self.x = x
+        self.y = y
+
+    def cambiar_coordenada(self, coorx, coory):
+        self.x = self.coorx
+        self.y = self.coory
+    
+Coordenada = Punto(3,6)
+Coora = Punto.cambiar_coordenada
+print (Coora)
+print (Coordenada.x)
+print (Coordenada.y)
